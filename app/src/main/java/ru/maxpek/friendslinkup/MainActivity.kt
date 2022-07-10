0635e26026f8e97219c13717abfd5cb904147ae6
MaximZavgorodnev <maxpek9@yandex.ru>
2022-07-10 17:20:44 +0300

    Создан кастомный Toolbar

--- app/src/main/java/ru/maxpek/friendslinkup/MainActivity.kt
+++ app/src/main/java/ru/maxpek/friendslinkup/MainActivity.kt
@@ -1,11 +1,19 @@
 package ru.maxpek.friendslinkup
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("RestrictedApi", "ResourceType", "UseSupportActionBar")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        hideActionBar()
+    }
+
+    private fun hideActionBar() {
+        supportActionBar?.hide()
     }
 }
