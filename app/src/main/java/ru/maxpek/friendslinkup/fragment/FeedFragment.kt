c1ed7c447418c35497558bd9a4857101ee79d926
MaximZavgorodnev <maxpek9@yandex.ru>
2022-07-11 14:08:29 +0300

    Созданы необходимые фрагменты

---
+++ app/src/main/java/ru/maxpek/friendslinkup/fragment/FeedFragment.kt
@@ -0,0 +1,10 @@
+package ru.maxpek.friendslinkup.fragment
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
+@ExperimentalCoroutinesApi
+@AndroidEntryPoint
+class FeedFragment : Fragment() {
+}
---
+++ app/src/main/java/ru/maxpek/friendslinkup/fragment/OpenPostFragment.kt
@@ -0,0 +1,6 @@
+package ru.maxpek.friendslinkup.fragment
+
+import androidx.fragment.app.DialogFragment
+
+class OpenPostFragment: DialogFragment() {
+}
