f56a9d11df19025021d8b6c7a54944a7cb439b0a
MaximZavgorodnev <maxpek9@yandex.ru>
2022-07-14 20:45:56 +0300

    Добавил BottomNavigation

--- app/src/main/java/ru/maxpek/friendslinkup/AppActivity.kt
+++ app/src/main/java/ru/maxpek/friendslinkup/AppActivity.kt
@@ -3,15 +3,18 @@ package ru.maxpek.friendslinkup
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class AppActivity : AppCompatActivity() {
+    private lateinit var bottomNavView: BottomNavigationView
+
     @SuppressLint("RestrictedApi", "ResourceType", "UseSupportActionBar")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        lateinit var binding: Bindin
+        setContentView(binding.root)
         hideActionBar()
+
     }
 
     private fun hideActionBar() {
