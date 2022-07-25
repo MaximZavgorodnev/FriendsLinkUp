ce572c2b9e05b22091b69d785eebd4f8edb4a72a
MaximZavgorodnev <maxpek9@yandex.ru>
2022-07-25 18:30:40 +0300

    Переделал переключение фрагментов на Navigation drawer

--- app/src/main/java/ru/maxpek/friendslinkup/AppActivity.kt
+++ app/src/main/java/ru/maxpek/friendslinkup/AppActivity.kt
@@ -7,9 +7,12 @@ import android.view.MenuItem
 import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBar
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.transition.Visibility
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -37,7 +40,6 @@ class AppActivity : AppCompatActivity() {
     companion object{
         private const val MAPKIT_API_KEY = "e0f40ead-fefb-45cf-821c-37efc0eaa548"
     }
-    private lateinit var bottomNavView: BottomNavigationView
     lateinit var binding: ActivityAppBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,17 +47,29 @@ class AppActivity : AppCompatActivity() {
         MapKitFactory.setApiKey(MAPKIT_API_KEY)
         binding = ActivityAppBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        hideActionBar()
-        bottomNavView = binding.bottomNavigation
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val feedFragment = FeedFragment()
         val eventFragment = EventFragment()
         val jobFragment = JobFragment()
 
         setThatFragment(feedFragment)
+//        setSupportActionBar(findViewById(R.id.topAppBar))
 
-        bottomNavView.setOnItemSelectedListener {
-            when(it.itemId){
+//        supportActionBar?.setIcon(R.drawable.ic_menu_24)
+//        supportActionBar?.setHomeActionContentDescription(R.drawable.ic_menu_24)
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_menu_24)
+
+
+//        supportActionBar.lis
+//            binding.drawerLayout.open()
+//        }
+
+        binding.navigationView.setNavigationItemSelectedListener { menuItem ->
+            // Handle menu item selected
+
+            when(menuItem.itemId){
                 R.id.posts ->{
                     setThatFragment(feedFragment)
                 }
@@ -66,38 +80,42 @@ class AppActivity : AppCompatActivity() {
                     setThatFragment(eventFragment)
                 }
             }
+            binding.drawer.closeDrawer(GravityCompat.START)
+            menuItem.isChecked = true
             true
         }
 
 
-        binding.menu.setOnClickListener {
-            PopupMenu(it.context, it).apply {
-                menuInflater.inflate(R.menu.menu_main, menu)
-                menu.let {menuVisible ->
-                    menuVisible.setGroupVisible(R.id.unauthenticated, !viewModel.authenticated)
-                    menuVisible.setGroupVisible(R.id.authenticated, viewModel.authenticated)
-            }
-//                inflate(R.menu.menu_main)
-                setOnMenuItemClickListener { item ->
-                    when (item.itemId) {
-                        R.id.signin -> {
-                            findNavController(R.id.frame).navigate(R.id.action_feedFragment_to_authenticationFragment)
-                            true
-                        }
-                        R.id.signup -> {
-                            findNavController(R.id.frame).navigate(R.id.action_feedFragment_to_registrationFragment)
-                            true
-                        }
-                        R.id.signout -> {
-                            appAuth.removeAuth()
-                            true
-                        }
 
-                        else -> false
-                    }
-                }
-            }.show()
-        }
+
+//        binding.menu.setOnClickListener {
+//            PopupMenu(it.context, it).apply {
+//                menuInflater.inflate(R.menu.menu_main, menu)
+//                menu.let {menuVisible ->
+//                    menuVisible.setGroupVisible(R.id.unauthenticated, !viewModel.authenticated)
+//                    menuVisible.setGroupVisible(R.id.authenticated, viewModel.authenticated)
+//            }
+////                inflate(R.menu.menu_main)
+//                setOnMenuItemClickListener { item ->
+//                    when (item.itemId) {
+//                        R.id.signin -> {
+//                            findNavController(R.id.frame).navigate(R.id.action_feedFragment_to_authenticationFragment)
+//                            true
+//                        }
+//                        R.id.signup -> {
+//                            findNavController(R.id.frame).navigate(R.id.action_feedFragment_to_registrationFragment)
+//                            true
+//                        }
+//                        R.id.signout -> {
+//                            appAuth.removeAuth()
+//                            true
+//                        }
+//
+//                        else -> false
+//                    }
+//                }
+//            }.show()
+//        }
 
         viewModel.data.observe(this) {
             invalidateOptionsMenu()
@@ -105,6 +123,7 @@ class AppActivity : AppCompatActivity() {
 
         checkGoogleApiAvailability()
     }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -114,6 +133,30 @@ class AppActivity : AppCompatActivity() {
             it.setGroupVisible(R.id.authenticated, viewModel.authenticated)
         }
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        binding.drawer.isOpen
+        return when (item.itemId) {
+            R.id.signin -> {
+                findNavController(R.id.frame).navigate(R.id.action_feedFragment_to_authenticationFragment)
+                true
+            }
+            R.id.signup -> {
+                findNavController(R.id.frame).navigate(R.id.action_feedFragment_to_registrationFragment)
+                true
+            }
+            R.id.signout -> {
+                appAuth.removeAuth()
+                true
+            }
+            android.R.id.home -> {
+                if (binding.drawer.isOpen) binding.drawer.closeDrawer(GravityCompat.START) else binding.drawer.openDrawer(GravityCompat.START)
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     private fun checkGoogleApiAvailability() {
