c836267683544058cff2baffb491a970e4b22388
MaximZavgorodnev <maxpek9@yandex.ru>
2022-10-06 18:01:03 +0300

    Начал работу над созданием работы. Проблема с вводом даты. Нужно переопределить правильный формат

--- app/src/main/java/ru/maxpek/friendslinkup/fragment/NewJobFragment.kt
+++ app/src/main/java/ru/maxpek/friendslinkup/fragment/NewJobFragment.kt
@@ -4,12 +4,14 @@ import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.os.Build
 import android.os.Bundle
-import android.text.method.DateTimeKeyListener
+import android.text.Layout
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
+import android.view.View.OnTouchListener
 import android.view.ViewGroup
-import android.widget.CalendarView
 import android.widget.DatePicker
+import android.widget.EditText
 import androidx.activity.addCallback
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
@@ -22,6 +24,7 @@ import ru.maxpek.friendslinkup.R
 import ru.maxpek.friendslinkup.databinding.FragmentNewJobBinding
 import ru.maxpek.friendslinkup.util.GoDataTime
 import ru.maxpek.friendslinkup.viewmodel.JobViewModel
+import java.text.SimpleDateFormat
 import java.util.*
 
 
@@ -37,7 +40,9 @@ var savedYear = 0
 class NewJobFragment: Fragment(), DatePickerDialog.OnDateSetListener {
     private val viewModel: JobViewModel by activityViewModels()
     @RequiresApi(Build.VERSION_CODES.O)
-    @SuppressLint("FragmentBackPressedCallback", "UseRequireInsteadOfGet")
+    @SuppressLint("FragmentBackPressedCallback", "UseRequireInsteadOfGet",
+        "ClickableViewAccessibility"
+    )
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -66,44 +71,79 @@ class NewJobFragment: Fragment(), DatePickerDialog.OnDateSetListener {
             it.link.let(binding.link::setText)
         }
 
-
-        binding.start.setOnEditorActionListener { textView, actionId, keyEvent ->
-
-            if (textView.isInputMethodTarget){
-                println("111111100000000")
+//        binding.start.onFocusChangeListener = View.OnFocusChangeListener { view, b ->
+//            if(binding.start.hasFocus()){
+//                getDataCalendar()
+//                DatePickerDialog(context!!, this, year, month, day).show()
+//            }
+//        }
+        binding.start.setOnTouchListener { viewStart, event ->
+            viewStart.isFocusable = true
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    val layout: Layout = (viewStart as EditText).layout
+                    val x: Float = event.x + viewStart.scrollX
+                    val offset: Int = layout.getOffsetForHorizontal(0, x)
+                    if (offset > 0) if (x > layout.getLineMax(0)) viewStart.setSelection(
+                        offset
+                    ) else viewStart.setSelection(offset - 1)
+                    getDataCalendar()
+                    DatePickerDialog(context!!, this, year, month, day).show()
+                }
             }
-            println(keyEvent)
-//           if (actionId == EditorInfo.){
-//               println("111111100000000")
-//           }
-//            println("111111100000000")
             true
         }
 
-        binding.button.setOnClickListener {
-            getDataCalendar()
-            DatePickerDialog(context!!, this, year, month, day).show()
-
+        binding.end.setOnTouchListener { v, event ->
+            v.isFocusable = true
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    val layout: Layout = (v as EditText).layout
+                    val x: Float = event.x + v.scrollX
+                    val offset: Int = layout.getOffsetForHorizontal(0, x)
+                    if (offset > 0) if (x > layout.getLineMax(0)) v.setSelection(
+                        offset
+                    ) else v.setSelection(offset - 1)
+                    getDataCalendar()
+                    DatePickerDialog(context!!, this, year, month, day).show()
+                }
+            }
+            true
         }
+
         return binding.root
     }
 
     private fun getDataCalendar(){
-        val cal = Calendar.getInstance()
-        day   = cal.get(Calendar.DAY_OF_MONTH)
-        month = cal.get(Calendar.MONTH)
-        year  = cal.get(Calendar.YEAR)
+        if (day == 0){
+            val cal = Calendar.getInstance()
+            day = cal.get(Calendar.DAY_OF_MONTH)
+            month = cal.get(Calendar.MONTH)
+            year  = cal.get(Calendar.YEAR)
+        }
     }
 
     private fun pickDate(){
 
     }
 
-    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
-        savedDay = dayOfMonth
-        savedMonth = month
-        savedYear = year
+    @RequiresApi(Build.VERSION_CODES.O)
+    @SuppressLint("SimpleDateFormat")
+    override fun onDateSet(view: DatePicker?, yearOf: Int, monthOf: Int, dayOfMonth: Int) {
+        day = dayOfMonth
+        month = monthOf
+        year = yearOf
+        val date = listOf(day, month, year)
+        GoDataTime.convertDataInput(date)
+//            "$day/$month/$year"
+//        val parsedDate = LocalDateTime.parse(date, DateTimeFormatter.ISO_DATE_TIME)
 
-        println("$savedDay  $savedMonth  $savedYear  " )
+        val dateString = "$day.$month.$year"
+
+        val formatter = SimpleDateFormat("DD.mm.yyyy")
+        val date1 = formatter.parse(dateString)
+
+        println(date1)
+        println(dateString)
     }
 }
--- app/src/main/java/ru/maxpek/friendslinkup/util/GoDataTime.kt
+++ app/src/main/java/ru/maxpek/friendslinkup/util/GoDataTime.kt
@@ -1,9 +1,12 @@
 package ru.maxpek.friendslinkup.util
 
 import android.annotation.SuppressLint
+import ru.maxpek.friendslinkup.fragment.year
 
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.util.ArrayList
+
 @SuppressLint("NewApi")
 object GoDataTime {
     fun convertDataTime(dateTime: String): String {
@@ -23,5 +26,20 @@ object GoDataTime {
             val parsedDate = LocalDateTime.parse(dateTime, DateTimeFormatter.ISO_DATE_TIME)
             parsedDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
         }
+    }
+
+    fun convertDataInput(date: List<Int>): String{
+        val x = date[0]
+        val y = date[1]
+        val z = date[2]
+        val day = when (x){
+            in 1..9  -> "0$x"
+            else -> {"$x"}
+        }
+
+
+        val month: String
+        val year: String
+        return day
     }
 }
